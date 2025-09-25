241cfae14baf91510957393e5821e9b6a977ddaa
annisa :b <annisaputriaprilia12@gmail.com>
2025-09-25 23:14:28 +0700

    splashScreen, fluttersvg

---
+++ lib/screens/splashScreen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 4), () {
+      Navigator.pushReplacementNamed(context, '/home');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white, // bisa diganti warna sesuai tema
+      body: Center(
+        child: Lottie.asset(
+          'assets/json/phoenix.json',
+          width: 200,   
+          height: 200,
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
+  }
+}
