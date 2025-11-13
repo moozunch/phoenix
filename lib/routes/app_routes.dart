d0f7e86952687b3a8626b276d67e88c417cdbf49
Sabitwise <atikahss2006@gmail.com>
2025-11-13 13:13:14 +0700

    chore: update flutter dependencies

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:phoenix/screens/splashScreen.dart';
+import 'package:phoenix/screens/HomePage.dart';
+
+class AppRoutes {
+  static const splash = '/';
+  static const home = '/home';
+}
